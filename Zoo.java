28f1b088111d645ff9c5d98178e55ecaabbe67ab
tekater <120014627+tekater@users.noreply.github.com>
2022-12-28 12:44:10 +0900

    Add files via upload

---
+++ Zoo.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+System.out.println("Под каким номером животное ты хочешь увидеть? 1-6");
+int zoo = scanner.nextInt();
+        if (zoo == 1) {
+        System.out.println(camel);
+        } else if (zoo == 2) {
+        System.out.println(lion);
+		} else if (zoo == 3) {
+        System.out.println(deer);
+        } else if (zoo == 4) {
+        System.out.println(goose);
+        } else if (zoo == 5) {
+        System.out.println(bat);
+        } else if (zoo == 6) {
+        System.out.println(rabbit);
+        }else {
+		System.out.println("Вы ввели неверное число");
+		}
+	}
+}
